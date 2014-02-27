3674c1f95526df4af30fefa57e6ca8c270aeded8
Hack Reactor <hackreactor@HR-A4.local>
2014-02-27 10:00:19 -0800

    prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,36 @@
 var makeQueue = function() {
   // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.length = 0;
+  instance.storage = {};
+
+  // Implement the methods below
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value){
+
+    this.storage[this.length++] = value;
+
+  },
+  dequeue: function(){
+    if(this.length > 0) {
+      var item = this.storage[0];
+      for(var num = 1; num < this.length; num++) {
+        this.storage[num-1] = this.storage[num];
+      }
+      delete this.storage[this.length-1];
+      this.length--;
+
+      return item;
+    }
+  },
+  size: function(){
+    return this.length;
+  }
+
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,32 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  instance.length = 0;
+  instance.storage = {};
+
+  return instance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push : function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+
+  pop : function(){
+    if(this.length > 0){
+      var item = this.storage[this.length-1];
+      delete this.storage[this.length-1];
+      this.length--;
+      return item;
+    }
+  },
+
+  size : function(){
+    return this.length;
+  }
+
+};
