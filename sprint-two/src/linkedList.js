45dd75d59052f0caf46d8f2fa94028fc4321ac4d
Hack Reactor <hackreactor@HR-A4.local>
2014-02-27 11:41:13 -0800

    start linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(this.head === null){
+      this.head = new makeNode(value);
+      this.tail = this.head;
+    } else{
+      this.tail.next = new makeNode(value);
+      this.tail = this.tail.next;
+    }
+
   };
 
   list.removeHead = function(){
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+    else{
+      this.head = this.head.next;
+    }
   };
 
   list.contains = function(target, node){
+    node = node || head;
+    while(node[next] !== null) {
+      if(node[value] === target) {
+        return true;
+      }
+      else if(node.next[value] === target) {
+        return true;
+      }
+      node = node[next];
+    }
+    return false;
   };
 
   return list;
