29ad485c3b421728c7e8fa08e1260597657f77ee
Hack Reactor <hackreactor@HR-A4.local>
2014-02-27 17:23:31 -0800

    trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,5 +1,5 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
   newTree.children = undefined;
   return newTree;
@@ -9,8 +9,25 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  if(this.children === undefined) {
+    this.children = [makeTree(value)];
+  }
+  else {
+    this.children.push(makeTree(value));
+  }
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target) {
+    return true;
+  }
+  if(this.children !== undefined){
+    for(var i = 0; i < this.children.length; ++i) {
+      if(this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
