71a7d49126c217a4ad3edd5a578468bf0d709049
Hack Reactor <hackreactor@HR-A4.local>
2014-02-27 20:58:54 -0800

    makeSet

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,24 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage === undefined){
+    this._storage = {};
+  }
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage === undefined){
+    return false;
+  }
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item){
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
+  if(Object.keys(this._storage).length === 0){
+    this._storage = undefined;
+  }
 };
