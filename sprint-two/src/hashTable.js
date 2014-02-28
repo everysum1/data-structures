f3dd901c28b7218c32a47b967c4b2277b56cd9cd
Hack Reactor <hackreactor@HR-A4.local>
2014-02-28 14:41:08 -0800

    hashes

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,22 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.set(index, value);
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key){
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  return this._storage.get(index);
 };
 
-HashTable.prototype.remove = function(k){
+HashTable.prototype.remove = function(key){
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.each(function(val, ki, stor) {
+    if(index === ki) {
+      stor[ki] = null;
+    }
+  });
 };
 
