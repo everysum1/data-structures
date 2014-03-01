b496c8372692419b1fd80cdb375412aeeaaf18d9
Hack Reactor <hackreactor@HR-A4.local>
2014-02-28 16:05:18 -0800

    tree with parent links and remove from parent method & tree specs

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -19,6 +19,29 @@ describe("tree", function() {
     expect(tree.children[0].value).to.equal(5);
   });
 
+  it("should have a child that recognizes its parent", function(){
+    tree.addChild(5);
+    tree.children[0].addChild(3);
+    expect(tree.children[0].children[0].parent.value).to.equal(5);
+  });
+
+  it("should dissacoiate a parent child relationship when removeFromParent is called", function(){
+    var child;
+    tree.addChild(5);
+    tree.addChild(4);
+    child = tree.children[0];
+    expect(tree.children[0].value).to.equal(5);
+
+    tree.children[0].removeFromParent();
+    expect(tree.children.length).to.equal(1);
+    expect(tree.children[0].value).to.equal(4);
+    expect(child.parent).to.equal(undefined);
+
+    tree.children[0].removeFromParent();
+    expect(tree.children).to.equal(undefined);
+
+  });
+
   it("should return true for a value that the tree contains", function(){
     tree.addChild(5);
     assert.isTrue(tree.contains(5));
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = Object.create(treeMethods);
   newTree.value = value;
   newTree.children = undefined;
+  newTree.parent = undefined;
   return newTree;
 };
 
@@ -9,12 +10,27 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var child = makeTree(value);
+  child.parent = this;
   if(this.children === undefined) {
-    this.children = [makeTree(value)];
+    this.children = [child];
   }
   else {
-    this.children.push(makeTree(value));
+    this.children.push(child);
   }
+};
+
+treeMethods.removeFromParent = function(){
+  for(var i = 0; i < this.parent.children.length; ++i) {
+    if(this.parent.children[i] === this) {
+      this.parent.children.splice(i, 1);
+      break;
+    }
+  }
+  if(this.parent.children.length === 0) {
+    this.parent.children = undefined;
+  }
+  this.parent = undefined;
 };
 
 treeMethods.contains = function(target){
